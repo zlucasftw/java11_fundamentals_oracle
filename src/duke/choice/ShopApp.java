4d8e90e79ad393631c4f2269c6a8c1c6a5092ea9
zlucasftw <ilucasmcpvp@gmail.com>
2025-04-21 20:01:05 -0300

    add: Exercise 4_02

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -18,6 +18,8 @@ public class ShopApp {
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
 
+        Clothing[] items = {item1, item2};
+
         item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
