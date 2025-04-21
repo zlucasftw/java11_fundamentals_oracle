08fcd09a5f8e3ff698541e1b168bdc36b3c5abfe
zlucasftw <ilucasmcpvp@gmail.com>
2025-04-21 13:46:05 -0300

    fix: Instructor answer

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -1,10 +1,16 @@
 package duke.choice;
 
 public class ShopApp {
+
     public static void main(String[] args) {
-        Customer c1 = new Customer();
-        c1.name = "Pinky";
 
         System.out.println("Welcome to Duke Choice Shop!");
+
+        Customer c1 = new Customer();
+
+        c1.name = "Pinky";
+
+        System.out.println("Customer is " + c1.name);
+
     }
 }
