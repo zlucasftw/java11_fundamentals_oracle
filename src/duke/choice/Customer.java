935a936d1ca748deef8ec86e7c2f7d49c2260559
zlucasftw <ilucasmcpvp@gmail.com>
2025-04-21 12:56:44 -0300

    add: Exercise 2

---
+++ src/duke/choice/Customer.java
@@ -0,0 +1,5 @@
+package duke.choice;
+
+public class Customer {
+    String name;
+}
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -2,6 +2,9 @@ package duke.choice;
 
 public class ShopApp {
     public static void main(String[] args) {
+        Customer c1 = new Customer();
+        c1.name = "Pinky";
+
         System.out.println("Welcome to Duke Choice Shop!");
     }
 }
