0e04ab75a552672c72c3663111f7d305c4f2962a
zlucasftw <ilucasmcpvp@gmail.com>
2025-05-04 18:52:31 -0300

    add: Exercise 5_01

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -2,8 +2,36 @@ package duke.choice;
 
 public class Clothing {
 
-    String description;
-    double price;
-    String size = "M";
+    public final double MINIMUM_PRICE = 10;
+    public final double TAX_RATE = 0.2;
 
+    private String description;
+    private double price;
+    private String size = "M";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price * (this.TAX_RATE + 1);
+    }
+
+    public void setPrice(double price) {
+        if (price >= this.MINIMUM_PRICE) {
+            this.price = price;
+        }
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
