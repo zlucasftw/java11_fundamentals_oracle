9d81bfcc6e9784882dae5a5cc0de2a86aecc772c
zlucasftw <ilucasmcpvp@gmail.com>
2025-05-11 02:15:18 -0300

    add: instructor solution 6_3

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -6,8 +6,8 @@ public class Clothing {
     private double price;
     private String size = "M";
 
-    public static final double MIN_PRICE = 10;
-    public static final double MIN_TAX = .2;
+    public final static double MIN_PRICE = 10;
+    public final static double TAX_RATE = .2;
 
     public Clothing(String description, double price, String aSize) {
         this.description = description;
@@ -24,7 +24,7 @@ public class Clothing {
     }
 
     public double getPrice() {
-        return price + (price * MIN_TAX);
+        return price + (price * TAX_RATE);
     }
 
     public void setPrice(double price) {
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -15,7 +15,7 @@ public class ShopApp {
         // c1.setName("Pinky");
         // c1.setSize("S");
 
-        System.out.println("The minimum price value for clothing is: $" + Clothing.MIN_PRICE);
+        System.out.println("Min Price " + Clothing.MIN_PRICE);
 
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
