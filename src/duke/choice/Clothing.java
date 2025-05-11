4cc9b7d63b6031431e20eb39c69e0b7920731bf0
zlucasftw <ilucasmcpvp@gmail.com>
2025-05-11 02:12:21 -0300

    add: Exercise 6_3

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -6,8 +6,8 @@ public class Clothing {
     private double price;
     private String size = "M";
 
-    private final double MIN_PRICE = 10;
-    private final double MIN_TAX = .2;
+    public static final double MIN_PRICE = 10;
+    public static final double MIN_TAX = .2;
 
     public Clothing(String description, double price, String aSize) {
         this.description = description;
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -15,6 +15,8 @@ public class ShopApp {
         // c1.setName("Pinky");
         // c1.setSize("S");
 
+        System.out.println("The minimum price value for clothing is: $" + Clothing.MIN_PRICE);
+
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
